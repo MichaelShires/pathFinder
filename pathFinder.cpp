84300407e033878c1b9711f74b1791c1bc0f5cd5
Michael Shires <mshires1@gmail.com>
2014-10-20 23:11:58 -0400

    going to sleep for now, hopefully work some more tomorrow, trying to come up with a nice while loop or two to get the csv file quickly parsed into the 2d array.  figure defining the amount of line segs was a nice touch

--- pathFinder.cpp
+++ pathFinder.cpp
@@ -1,5 +1,8 @@
 #include <iostream> 
-#include <cstdlib> 
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#define LINE_SEG 4
 using namespace std;
 
 /*
@@ -7,11 +10,17 @@ using namespace std;
  * runs off of an insanely basic map at the moment.
  */
 int main(int argc, char* argv[]) { 
+	int mapData[LINE_SEG][3];
 	//each element of the argv array after zero is a space delimited
-	//input.  this allows you to input two integers and then have
+	//input.  this allows/forces you to input two integers and then have
 	//them stored.  pretty cool amirite?
 	int startLocation = atoi(argv[1]);
 	int endLocation = atoi(argv[2]);
-	cout << startLocation << endLocation << endl;
+	cout << startLocation << ' ' << endLocation << endl;
+	string data;
+	ifstream map ("map.csv");
+	getline(map, data, ',');
+
+	cout << data << endl;
 	return 0; 
 }
